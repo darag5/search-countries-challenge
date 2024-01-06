3228e609933422f87c637e8f7e7496e85d843b89
darag5 <ara.gonzalez92@gmail.com>
2024-01-06 18:55:48 -0300

    feat: env vars using dotenv

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
 
 app.get('/', (_, res) => {
   res.send('Server running');
 });
 
-app.listen(8000, () => {
-  console.log('Server running');
+const PORT = process.env.PORT;
+app.listen(() => {
+  console.log(`Server running http://localhost:${PORT}`);
 });
