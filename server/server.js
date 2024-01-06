aa50e39179123ee8a642fff5c3f9b9967e229053
darag5 <ara.gonzalez92@gmail.com>
2024-01-06 20:31:46 -0300

    chore: mongoose, nodemon, connect to db

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 const app = express();
 
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
 app.get('/', (_, res) => {
   res.send('Server running');
 });
 
-const PORT = process.env.PORT;
-app.listen(() => {
-  console.log(`Server running http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB(MONGODB_URI);
+    app.listen(PORT, () => {
+      console.log(`Server running http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
