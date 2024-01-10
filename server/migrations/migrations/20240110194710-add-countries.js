098e282dde508e186fe5f102bd9c9c48db51a256
darag5 <ara.gonzalez92@gmail.com>
2024-01-10 17:08:10 -0300

    feat(server): migrations

---
+++ server/migrations/migrations/20240110194710-add-countries.js
@@ -0,0 +1,11 @@
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const countries = require('../../../data/countries.json');
+
+export const up = async (db, client) => {
+  await db.collection('countries').insertMany(countries);
+};
+
+export const down = async (db, client) => {
+  await db.collection('countries').drop();
+};
