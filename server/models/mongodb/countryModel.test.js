f70695fd097a96c4e39938e9167207948b520a60
darag5 <ara.gonzalez92@gmail.com>
2024-01-12 12:45:40 -0300

    test(server): add unit test api

---
+++ server/models/mongodb/countryModel.test.js
@@ -0,0 +1,16 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert/strict';
+// import CountryModel from './countryModel';
+
+describe('Country Model', () => {
+  it('getAll', async (context) => {
+    // const findResult = {};
+    // const find = jest.fn().mockImplementation(() => findResult);
+    // jest.mock('./schemas/Country.js', () => ({ find }));
+    // const result = await CountryModel.getAll();
+    // expect(find).toHaveBeenCalledOnce();
+    // expect(result).toBe(findResult);
+
+    assert.equal(true, true);
+  });
+});
