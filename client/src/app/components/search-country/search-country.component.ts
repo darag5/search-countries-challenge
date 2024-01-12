fb675204d8f19356454fa89cb637abf8cf9bd8af
darag5 <ara.gonzalez92@gmail.com>
2024-01-12 01:07:47 -0300

    fix(client): submit form

--- client/src/app/components/search-country/search-country.component.ts
+++ client/src/app/components/search-country/search-country.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 // models
 import { Country } from 'src/app/models/country.model';
@@ -13,13 +13,17 @@ import { CountryService } from 'src/app/services/country.service';
   styleUrls: ['./search-country.component.css'],
 })
 export class SearchCountryComponent {
-  filterText = new FormControl('');
+  searchForm!: FormGroup;
+  submitted = false;
   countries: Country[] = [];
   filteredCountries: Country[] = [];
   showPopulationTotalPercentage = false;
   countryService = inject(CountryService);
 
   constructor() {
+    this.searchForm = new FormGroup({
+      filterText: new FormControl('', Validators.minLength(3)),
+    });
     this.getAllCountries();
   }
 
@@ -34,19 +38,30 @@ export class SearchCountryComponent {
     });
   }
 
+  get f() {
+    return this.searchForm.controls;
+  }
+
   filterResults() {
-    if (!this.filterText.value || this.filterText.value.length < 3) {
+    this.submitted = true;
+    if (
+      this.searchForm.invalid ||
+      !this.f['filterText'].value ||
+      this.f['filterText'].value.length < 3
+    ) {
       this.filteredCountries = this.countries;
       this.showPopulationTotalPercentage = false;
       return;
     }
 
-    this.countryService.getFilteredCountries(this.filterText.value).subscribe({
-      next: (data) => {
-        this.filteredCountries = data;
-        this.showPopulationTotalPercentage = true;
-      },
-      error: (error) => console.log(error),
-    });
+    this.countryService
+      .getFilteredCountries(this.f['filterText'].value)
+      .subscribe({
+        next: (data) => {
+          this.filteredCountries = data;
+          this.showPopulationTotalPercentage = true;
+        },
+        error: (error) => console.log(error),
+      });
   }
 }
