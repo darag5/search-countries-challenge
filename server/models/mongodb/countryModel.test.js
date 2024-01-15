dcc44f0c548ab9ee8bec8939213df4dcf7b883a1
darag5 <ara.gonzalez92@gmail.com>
2024-01-15 17:46:41 -0300

    test(server): add unit tests using jest

--- server/models/mongodb/countryModel.test.js
+++ server/models/mongodb/countryModel.test.js
@@ -1,16 +1,70 @@
-import { describe, it } from 'node:test';
-import assert from 'node:assert/strict';
-// import CountryModel from './countryModel';
+import { expect, jest, beforeEach } from '@jest/globals';
+import { CountryModel } from './countryModel';
+import { Country } from './schemas/Country';
 
 describe('Country Model', () => {
-  it('getAll', async (context) => {
-    // const findResult = {};
-    // const find = jest.fn().mockImplementation(() => findResult);
-    // jest.mock('./schemas/Country.js', () => ({ find }));
-    // const result = await CountryModel.getAll();
-    // expect(find).toHaveBeenCalledOnce();
-    // expect(result).toBe(findResult);
+  beforeEach(() => jest.restoreAllMocks());
 
-    assert.equal(true, true);
+  it('getAll', async () => {
+    jest.spyOn(Country, Country.find.name).mockResolvedValue([
+      {
+        _id: '65a0ab9655926def19649a2c',
+        keyName: 'IND',
+        name: 'India',
+        population: 1409902000,
+      },
+    ]);
+
+    const expected = [
+      {
+        _id: '65a0ab9655926def19649a2c',
+        keyName: 'IND',
+        name: 'India',
+        population: 1409902000,
+      },
+    ];
+
+    const result = await Country.find();
+
+    expect(Country.find).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(expected);
+  });
+
+  it('getPopulationTotal', () => {
+    const populationReducer = (testArray) => {
+      return testArray.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.population,
+        0
+      );
+    };
+
+    const emptyCountries = populationReducer([]);
+    const countriesWithPopulation = populationReducer([{ population: 10 }]);
+
+    expect(emptyCountries).toEqual(0);
+    expect(countriesWithPopulation).toEqual(10);
+  });
+
+  it('add population percentage to each countries', () => {
+    const populationTotal = 1000;
+
+    const calculatePopulationPercentage = (countryPopulation) =>
+      (countryPopulation * 100) / populationTotal;
+
+    const transform = (testArray) => {
+      return testArray.map(({ name, population }) => ({
+        name,
+        population,
+        populationTotalPercentage: calculatePopulationPercentage(population),
+      }));
+    };
+
+    const transformed = transform([{ name: 'Argentina', population: 1000 }]);
+    const percentageCalculated = calculatePopulationPercentage(1000);
+
+    expect(transformed[0]).toHaveProperty('name');
+    expect(transformed[0]).toHaveProperty('population');
+    expect(transformed[0]).toHaveProperty('populationTotalPercentage');
+    expect(percentageCalculated).toEqual(100);
   });
 });
